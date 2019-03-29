59fd6ed6435375969be2740d19bb72d86c8e7f07
quinn <36234189+316968@users.noreply.github.com>
2019-03-29 13:04:49 -0400

    Add files via upload

---
+++ Contact.cs
@@ -0,0 +1,130 @@
+﻿/*
+*Quinn parker-joyes
+* ics4u
+* unit 2 summative
+* contact class, saves contact info, reads that info, and shows age of contact
+* march 29 2019
+*/
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace _316968_Unit2Summative
+{
+    public class Contact
+    {
+        // global variables
+        private string firstName;
+        private string lastName;
+        private int day;
+        private int month;
+        private int year;
+        private string email;
+        //private DateTime birthDate;
+        // temp is used to remove commas from the contact info, so the 
+        public string temp;
+        //public string[] temp;
+        public List<string> list = new List<string>();
+        //public int age;
+         //System.IO.StreamWriter sw = new System.IO.StreamWriter("savedFile.txt");
+        
+        public Contact (string fN, string lN, int d, int m, int y, string eA)
+        {
+            firstName = fN;
+            lastName = lN;           
+            day = d;
+            month = m;
+            year = y;
+            email = eA;
+        }
+        public void getAge()
+        {
+            int tempAge;
+
+            try
+            {
+                DateTime birthDay = new DateTime(year, month, day);
+                DateTime current = new DateTime();
+                current = DateTime.Today;
+                TimeSpan age = current.Subtract(birthDay);
+                int.TryParse(age.ToString(), out tempAge);
+                double ageRounded = Math.Floor(age.Days / 365.25);
+                MessageBox.Show("you are " + ageRounded.ToString() + " years old");
+               
+
+            }
+            catch
+            {
+                MessageBox.Show("Please enter valid date");
+                
+            }
+                
+                
+                
+            
+        }
+        public void SaveToFile()
+        {
+            
+            try
+            {
+                System.IO.StreamWriter sw = new System.IO.StreamWriter("savedFile.txt");
+                string tempFirstName = "";
+                string tempLastName = "";
+                string tempEmail = "";
+                if (firstName.Contains(',') || lastName.Contains(',') || email.Contains(','))
+                {
+                    for (int i = 0; i<firstName.Split(',').Length; i++)
+                    {
+                        tempFirstName += firstName.Split(',')[i];
+                    }
+                    for(int j = 0; j<lastName.Split(',').Length; j++)
+                    {
+                        tempLastName += lastName.Split(',')[j];
+                    }
+                    for(int h = 0; h < email.Split(',').Length; h++)
+                    {
+                        tempEmail += email.Split(',')[h];
+                    }
+                    
+                    firstName = tempFirstName;
+                }
+                string tempDay, tempMonth, tempYear;
+                tempDay = day.ToString();
+                tempMonth = month.ToString();
+                tempYear = year.ToString();
+               
+                temp = (firstName + "," + lastName + "," + day + "," + month + "," + year + "," + email).ToString();
+                sw.WriteLine(temp);
+                sw.Flush();
+                sw.Close();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+        public string[] ReadFromFile()
+        {   
+            try
+            {
+                System.IO.StreamReader streamReader = new System.IO.StreamReader("savedFile.txt");
+                string[] contactArray = streamReader.ReadLine().Split(',');
+                return contactArray;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return null;
+            }
+            
+        }
+
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,99 @@
+﻿/*
+ * quinn parker-joyes
+ * march 28 2019
+ * unit 2 summative
+ * stores contact info, shows age of user in years, and saved contact info and shows saved contact info on re opening of program
+ * 
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _316968_Unit2Summative
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            try
+            {
+                Contact contactPlaceholder = new Contact("temp", "temp", 0, 0, 0, "temp");
+                string[] contactinfo = contactPlaceholder.ReadFromFile();
+                txtFirstName.Text = contactinfo[0];
+                txtLastName.Text = contactinfo[1];
+                int.TryParse(contactinfo[2], out int day);
+                txtDay.Text = day.ToString();
+                int.TryParse(contactinfo[3], out int month);
+                txtMonth.Text = month.ToString();
+                int.TryParse(contactinfo[4], out int year);
+                txtYear.Text = year.ToString();
+                txtEmail.Text = contactinfo[5];
+            }
+            catch
+            {
+                MessageBox.Show("Sorry! NO INFORMATION FOUND, file missing >:(");
+            }
+        }
+
+        public void btnContact_Click(object sender, RoutedEventArgs e)
+        {
+            
+            try
+            {
+                
+                int Day, Month, Year;
+                int.TryParse(txtDay.Text, out Day);
+                int.TryParse(txtMonth.Text, out Month);
+                int.TryParse(txtYear.Text, out Year);
+                
+                Contact contactCurrent = new Contact(txtFirstName.Text, txtLastName.Text, Day, Month, Year, txtEmail.Text);
+                
+                contactCurrent.getAge();
+                
+                //contact.sw.WriteLine(contact.temp);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            int Day, Month, Year;
+            int.TryParse(txtDay.Text, out Day);
+            int.TryParse(txtMonth.Text, out Month);
+            int.TryParse(txtYear.Text, out Year);
+            Contact contactClosed = new Contact(txtFirstName.Text, txtLastName.Text, Day, Month, Year, txtEmail.Text);
+            contactClosed.SaveToFile();
+        }
+
+
+        /*private void btnAge_Click(object sender, RoutedEventArgs e)
+{
+
+}*/
+
+        /// do a thing where when the user enters info it checks if its already been added, and in the catch statement it runs the show age thing
+    }
+}
